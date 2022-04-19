73aff513fb5eebeedbc7aafe61375861c76d8b5b
Jennyfer-Soledade-Lima <92947930+Jennyfer-Soledade-Lima@users.noreply.github.com>
2022-04-19 18:50:11 -0300

    Add files via upload

---
+++ Categoria.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class Categoria {
+    private $idCategoria ="";
+    private $descricao ="";
+    
+    
+    private function __construct($descricao,$idCategoria){
+        $this->setDescricao($descricao);
+        $this->setIdCategoria($idCategoria);
+    }
+    
+    public function getIdCategoria(){
+        return $this->idCategoria;
+    }
+
+    public function setIdCategoria($idCategoria){
+        $this->idCategoria = $idCategoria;
+        return $this;
+    }
+
+    public function getDescricao(){
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao){
+        $this->descricao = $descricao;
+        return $this;
+    }
+
+}
+?>
